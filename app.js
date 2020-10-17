4bda13de6e716777022dd3c7b90f897ee39dfc07
Jake Engelen <jte1544@yahoo.com>
2020-10-17 02:26:56 -0600

    pushing build

--- app.js
+++ app.js
@@ -121,10 +121,6 @@ app.use(
 const users = require('./store/users.json');
 app.post('/api/authenticate', (req, res) => {
 
-  console.log("Authenticating user:");
-  console.log(req.body.username);
-  console.log(req.body.password);
-
   const {
     username,
     password
